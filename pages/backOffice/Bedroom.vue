55e122ffc9014629fea283e3d89c7e1a07db001d
Moumen Bourahla <moumen.bourahla@le-campus-numerique.fr>
2025-02-19 12:01:03 +0100

    récupération des images en BDD pour chaque news, pour l'instant, une news n'a qu'une image (relation en cours par Tristan et Bast

---
+++ pages/backOffice/Bedroom.vue
@@ -0,0 +1,27 @@
+<template>
+    <table>
+<tr v-for="bedroom in bedroomStore.data">
+    <td class="bedroom">{{bedroom.id}}</td>
+    <td>{{bedroom.number}}</td>
+</tr>
+
+    </table>
+</template>
+
+<script setup>
+import {useBedroomStore} from "~/store/bedroom";
+
+
+const bedroomStore = useBedroomStore();
+
+definePageMeta({
+  layout: 'back-office',
+})
+
+</script>
+
+<style scoped>
+.bedroom {
+    color: white;
+}
+</style>
